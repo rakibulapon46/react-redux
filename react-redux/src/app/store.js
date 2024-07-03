b93bcba25cebd0eab85b491a8c161421adca8e94
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-07-04 03:38:06 +0600

    add todos

--- react-redux/src/app/store.js
+++ react-redux/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countersReducer from "../features/counters/countersSlice"
 import postsReducer from "../features/posts/postsSlice";
+import todosReducer from "../features/todos/todosSlice";
 
 const store = configureStore({
     reducer: {
         counters: countersReducer,
         posts: postsReducer,
+        todos: todosReducer,
     }
 })
 
