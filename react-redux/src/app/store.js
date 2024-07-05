3a132ad8169fef3a04cf9c10df2a48594866a224
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-07-06 00:28:19 +0600

    add photos in store

--- react-redux/src/app/store.js
+++ react-redux/src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import countersReducer from "../features/counters/countersSlice"
 import postsReducer from "../features/posts/postsSlice";
 import todosReducer from "../features/todos/todosSlice";
+import photosReducer from "../features/photos/photosSlice";
 
 const store = configureStore({
     reducer: {
         counters: countersReducer,
         posts: postsReducer,
         todos: todosReducer,
+        photos: photosReducer,
     }
 })
 
