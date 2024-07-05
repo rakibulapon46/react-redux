ae6e5dac40a168eea5148343448bcf28a099eb3b
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-07-05 23:56:18 +0600

    add photosSlice

---
+++ react-redux/src/features/photos/photosSlice.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { getPhotos } from "./photosAPI"
+
+const initialState = {
+    photos: [],
+    isLoading: false,
+    isError: false,
+    error: null,
+}
+
+export const fetchPhotos = createAsyncThunk("photos/fetchPhotos",
+    async () => {
+        const photos = getPhotos();
+        return photos;
+    }
+)
+
+const photosSlice = createSlice({
+    name: "photos",
+    initialState,
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchPhotos.pending, (state) => {
+            state.isLoading = true;
+            state.isError = false;
+        })
+        .addCase(fetchPhotos.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.photos = action.payload;
+        })
+        .addCase(fetchPhotos.pending, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.error = action.error?.message;
+        })
+    }
+})
+
+
+export default photosSlice.reducer;
