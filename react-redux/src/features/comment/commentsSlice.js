c9798426f7593cf0a9e477727597208b7a3ef13a
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-07-08 02:02:13 +0600

    commentsSlice

--- react-redux/src/features/comment/commentsSlice.js
+++ react-redux/src/features/comment/commentsSlice.js
@@ -1,6 +1,39 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getComments } from './commentAPI';
+
 const initialState = {
     comments: [],
     isLoading: false,
     isError: false,
     error: null,
 }
+
+export const fetchComments = createAsyncThunk("comments/fetchComments",
+    async () => {
+        const comments = getComments();
+        return comments;
+    }
+) 
+
+const commentsSlice = createSlice({
+    name: 'comments',
+    initialState,
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchComments.pending, (state) => {
+            state.isLoading = true;
+            state.isError = false;
+        })
+        .addCase(fetchComments.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.comments = action.payload;
+        })
+        .addCase(fetchComments.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.error = action.error?.message;
+        })
+    }
+})
+
+export default commentsSlice.reducer;
