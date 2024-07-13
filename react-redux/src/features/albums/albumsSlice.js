37de054eb7f71182dc069370ed053adb9231bdd4
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-07-13 23:58:57 +0600

    albumsSlice

--- react-redux/src/features/albums/albumsSlice.js
+++ react-redux/src/features/albums/albumsSlice.js
@@ -1,10 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAlbums } from "./albumsAPI";
 
 const initialState = {
-    albums: [],
-    isLoading: false,
-    isError: false,
-    error: null,
-}
+  albums: [],
+  isLoading: false,
+  isError: false,
+  error: null,
+};
 
-const albumsSlice = createSlice(())
+export const fetchAlbums = createAsyncThunk("albums/fetchAlbums", async () => {
+  const albums = getAlbums();
+  return albums;
+});
+
+const albumsSlice = createSlice({
+  name: "albums",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchAlbums.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+    })
+    .addCase(fetchAlbums.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.albums = action.payload;
+    })
+    .addCase(fetchAlbums.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.error?.message
+    });
+  },
+});
+
+export default albumsSlice.reducer;
