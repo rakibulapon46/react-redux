5137441001d4791656501316b8e37a39fa0cf082
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-07-17 23:59:23 +0600

    store js

--- react-redux/src/app/store.js
+++ react-redux/src/app/store.js
@@ -7,6 +7,22 @@ import commentsReducer from './../features/comment/commentsSlice';
 import usersReducer from "../features/users/usersSlice";
 import albumsReducer from "../features/albums/albumsSlice";
 
+/*
+const store = configureStore({
+    reducer: {
+        counters: countersReducer,
+        posts: postsReducer,
+        todos: todosReducer,
+        photos: photosReducer,
+        comments: commentsReducer,
+        users: usersReducer,
+        albums: albumsReducer,
+    }
+})
+
+export default store;
+*/
+
 const store = configureStore({
     reducer: {
         counters: countersReducer,
